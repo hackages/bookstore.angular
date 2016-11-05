f281d1b7242284547325637ef2b9c3e0bb28c4a9
davyengone <engonenzedavy@yahoo.com>
2016-11-05 19:31:28 +0100

    initial commit

---
+++ modules/common/nav/navcontroller.js
@@ -0,0 +1,9 @@
+(function(){
+  'use strict';
+
+    function NavController() {
+      this.title = "Hackages";
+    }
+
+    angular.module('common').controller('NavController',NavController);
+})();
